dc7318840b072c5bfb3a2bfb4e2edfd33a5e32da
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2021-02-21 19:35:50 +0100

    pipeline

--- deployment.sh
+++ deployment.sh
@@ -6,12 +6,10 @@ mkdir -p $TARGET
 
 if [ ! -d $PIPELINE ]; then
   git clone $REPO $PIPELINE
-  cd $PIPELINE
-else
-  cd $PIPELINE
-  git pull
 fi
 
+cd $PIPELINE
+git pull
 
 [ $(cat previous-version.txt 2> /dev/null) ] && PREVIOUS=$(cat previous-version.txt) || PREVIOUS='NULL'
 CURRENT=$(git rev-parse HEAD)
@@ -20,6 +18,7 @@ echo 'PREVIOUS VERSION --> '$PREVIOUS;
 echo 'CURRENT VERSION --> '$CURRENT;
 
 if [ $CURRENT != $PREVIOUS ]; then
+  # Run pipeline
   cd $PIPELINE
   npm install
   npm run build
@@ -29,8 +28,15 @@ if [ $CURRENT != $PREVIOUS ]; then
   cp ./package.json         $TARGET
   cp ./package-lock.json    $TARGET
 
+  # Install deps
   cd $TARGET
   npm install --production
 
+  # Restart service
+  npm run pm2:delete
+  npm run pm2
+
+  # Save git commit
+  cd $PIPELINE
   git rev-parse HEAD > previous-version.txt
 fi
