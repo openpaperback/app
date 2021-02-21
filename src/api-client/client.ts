b59fe8ad33fff3468f5a91fa09c232272975279f
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2021-02-21 20:36:22 +0100

    feat: api url

--- src/api-client/client.ts
+++ src/api-client/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+import { config } from "../config";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: config.API_URL,
 });
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const config = {
   MONGO_URL: process.env.MONGO_URL || "mongodb://localhost:27017/openpaperback",
+  API_URL: process.env.API_URL || "http://localhost:3000/api",
 };
