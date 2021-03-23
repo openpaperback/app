9b9de279c68f354d5d2dc2fdd1713696f7fe89ec
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2021-03-23 22:41:48 +0100

    feat: added comments to test build process

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,11 +4,13 @@ import { middleware as cache } from "apicache";
 import { authorRoutes } from "./author/author.routes";
 
 export function router(app: Application) {
+  // Books
   app.get("/api/book", bookRoutes.list);
   app.get("/api/book/author/:authorId", bookRoutes.listByAuthor);
   app.get("/api/book/:bookId", bookRoutes.get);
   app.get("/api/book/:bookId/text", cache("5 minutes"), bookRoutes.getText);
 
+  // Authors
   app.get("/api/author/top", authorRoutes.getMostDownloaded);
   app.get("/api/author/:authorId", authorRoutes.get);
 }
